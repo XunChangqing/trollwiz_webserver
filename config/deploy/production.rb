47d1999c9e652e844f39ab209cf56430acaf85d0
xunchangqing <xunchangqing@qq.com>
2015-08-18 22:17:36 +0800

    use rvm

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
+set :rvm_ruby_version, '2.2.0'
 
 
 # role-based syntax
